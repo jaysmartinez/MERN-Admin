d80a12523a460ec0979f048b91b38c3806472721
Jayson Martinez <jaysmartinez@gmail.com>
2023-03-16 17:06:34 -0500

    added more pages

--- server/index.js
+++ server/index.js
@@ -14,7 +14,7 @@ import salesRoutes from './routes/sales.js';
 import User from './models/User.js';
 import Product from './models/Product.js';
 import ProductStat from './models/ProductStat.js';
-// import Transaction from './models/Transaction.js';
+import Transaction from './models/Transaction.js';
 // import OverallStat from './models/OverallStat.js';
 // import AffiliateStat from './models/AffiliateStat.js';
 import {
@@ -56,6 +56,7 @@ mongoose
 		/* ONLY ADDS DATA ONE TIME */
 		// Product.insertMany(dataProduct);
 		// ProductStat.insertMany(dataProductStat);
+		// Transaction.insertMany(dataTransaction);
 		// User.insertMany(dataUser);
 	})
 	.catch((error) => console.log(`${error} did not connect`));
